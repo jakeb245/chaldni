8819cda99cfe90fce50405be1c449ebbf3e085aa
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-02-06 17:49:55 -0500

    Stuff

--- src/compare_images.py
+++ src/compare_images.py
@@ -6,12 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, color
 from skimage.filters import threshold_mean
+from skimage.restoration import denoise_tv_chambolle
 from sklearn.metrics import mean_squared_error
-from skimage.transform import rescale
 
 
 def rgba_to_binary(image):
-    #rgb = color.rgba2rgb(image)
+    # rgb = color.rgba2rgb(image)
     gray = color.rgb2gray(image)
     thresh = threshold_mean(gray)
     return gray > thresh
@@ -23,24 +23,18 @@ def rgb_to_binary(image):
     return gray > thresh
 
 
-def really_long_array(array):
-    long = []
-    for x in range(len(array)):
-        for y in array[x]:
-            long.append(y)
-    return np.array(long)
-
-
-def binary_to_transparent(image,color):
+def binary_to_transparent(image, color=None):
     new_image = []
     for x in range(len(image)):
         new_x = []
         for y in range(len(image[x])):
-            if image[x][y]: # True is black
+            if image[x][y]:  # True is black
                 if color == 'red':
                     new_y = [255, 0, 0, 0.5]
                 elif color == 'blue':
                     new_y = [0, 0, 255, 0.5]
+                elif color == 'yellow':
+                    new_y = [255, 255, 0, 0.5]
                 else:
                     new_y = [255, 255, 255, 0.5]
             else:
@@ -51,25 +45,17 @@ def binary_to_transparent(image,color):
 
 
 if __name__ == '__main__':
-    exp = rgba_to_binary(io.imread('RealProto.png'))*1
+    exp_raw = io.imread('RealProto.png')
+    exp = rgba_to_binary(denoise_tv_chambolle(exp_raw, weight=0.2, channel_axis=-1))*1
     theo = rgb_to_binary(io.imread('TheoProto.png'))*1
 
-    exp_array = really_long_array(exp)
-    theo_array = really_long_array(theo)
-    exp_array = np.logical_not(theo_array).astype(int)
-    print(exp_array)
-    print(theo_array)
-
     print(mean_squared_error(exp, theo))
-    print(mean_squared_error(exp_array, theo_array))
-    print(mean_squared_error(theo_array, theo_array))
 
-    exp_test = binary_to_transparent(exp,'red')
-    theo_test = binary_to_transparent(theo,'blue')
+    exp_test = binary_to_transparent(exp)
+    theo_test = binary_to_transparent(theo, 'yellow')
 
     fig = plt.figure(dpi=200)
     plt.rcParams['figure.facecolor'] = 'black'
     plt.imshow(exp_test + theo_test)
     plt.axis('off')
-    #plt.savefig('AddedFigures.png',bbox_inches='tight', pad_inches=0)
-    plt.show()
+    plt.savefig('AddedFigures.png', bbox_inches='tight', pad_inches=0)
